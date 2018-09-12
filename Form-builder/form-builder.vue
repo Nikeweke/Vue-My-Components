40df2e97fc932c2d3ad7fc61dee65a83ed28e88c
Nikeweke <jxammet@gmail.com>
2018-09-12 13:16:54 +0300

    form-builder

--- Form-builder/form-builder.vue
+++ Form-builder/form-builder.vue
@@ -1,3 +1,74 @@
+<template>
+<span>
+    <!-- Dynamic Form Builder --> 
+    <div v-for="(item, i) of fields" :key="i"  class="form-group has-label">
+        <label>{{item.label}}</label> &nbsp;
+
+        <!-- Input - text, password -->
+        <input v-if="isInput(item.type)"
+               v-model="form[item.model]"
+               :placeholder="item.placeholder"
+               :type="item.type" class="input" />
+        
+
+        <!-- Select -->
+        <div class="select" v-if="isSelect(item.type)">
+            <select v-model="form[item.model]"
+                    class="select">
+                <option v-for="(opt, i) of item.options"
+                        :key="i"
+                        :value="opt.val">
+
+                        {{ opt.name }}  
+                </option>
+            </select>
+        </div>
+
+
+        <!-- Textarea -->
+        <textarea v-if="isTextarea(item.type)" 
+                  v-model="form[item.model]"
+                  class="textarea">
+        </textarea>
+
+        <!-- Checkboxes --> 
+        <template v-if="isCheckbox(item.type, item.model)">
+            <br /><br />
+            <div class="form-check checkbox-inline" v-for="(opt, i) of item.options" :key="i">
+            <label class="form-check-label">
+                <input class="form-check-input" type="checkbox" :value="opt.val" v-model="form[item.model]">
+                <span class="form-check-sign"></span> {{ opt.name }}
+            </label>
+            </div>
+        </template>
+
+        <!-- RadioButtons -->
+        <template v-if="isRadio(item.type)">
+            <br /><br />
+            <div v-for="(opt, i) of item.options" :key="i" class="form-check form-check-radio" >
+            <label  class="form-check-label">
+                <input class="form-check-input" :value="opt.val" type="radio" v-model="form[item.model]"> 
+                <span class="form-check-sign"></span> {{ opt.name }}
+            </label>
+            </div>
+        </template>
+
+        <!-- Datepicker -->
+        <datetime v-if="isDatepicker(item.type)" 
+                  :auto="dpConfig.auto"
+                  :class="dpConfig.theme"
+                  :format="dpConfig.format"
+                  :phrases="dpConfig.phrases"
+                  :input-class="dpConfig.classInput"
+                  v-model="form[item.model]" />
+
+    </div>
+
+    <code>{{ form }}</code>
+</span>
+</template>
+
+<script>
 /*
 *  FormBuilder / backend.js
 *
@@ -8,8 +79,6 @@
 * 'Fields' тоже под наблюдением, когда передаються новые поля надо схватывать новое значение формы из хранилища
 */
   
-import '@/assets/sass/vue-datetime/vue-datetime.scss'
-
 export default  {
 
   // PROPS
@@ -119,9 +188,6 @@ export default  {
     isDatepicker (fieldType) {
       return fieldType === 'datepicker'
     }
-  }
+  },
 }
-
-
-
-
+</script>
--- Form-builder/index.vue
+++ Form-builder/index.vue
@@ -1,58 +1,38 @@
 <template>
-<span>
-    <!-- Dynamic Form Builder --> 
-    <div v-for="(item, i) of fields" :key="i"  class="form-group has-label">
-        <label>{{item.label}}</label>
-
-        <!-- Input - text, password -->
-        <input v-if="isInput(item.type)" v-model="form[item.model]" :placeholder="item.placeholder" :type="item.type" class="form-control" />
-        
-        <!-- Select -->
-        <select v-if="isSelect(item.type)" v-model="form[item.model]" class="form-control">
-            <option v-for="(opt, i) of item.options"
-                    :key="i"
-                    :value="opt.val">
-
-                    {{ opt.name }}  
-            </option>
-        </select>
-
-        <!-- Textarea -->
-        <textarea v-if="isTextarea(item.type)" v-model="form[item.model]" class="form-control" style="height:100px"></textarea>
-
-        <!-- Checkboxes --> 
-        <template v-if="isCheckbox(item.type, item.model)">
-            <br /><br />
-            <div class="form-check checkbox-inline" v-for="(opt, i) of item.options" :key="i">
-            <label class="form-check-label">
-                <input class="form-check-input" type="checkbox" :value="opt.val" v-model="form[item.model]">
-                <span class="form-check-sign"></span> {{ opt.name }}
-            </label>
-            </div>
-        </template>
-
-        <!-- RadioButtons -->
-        <template v-if="isRadio(item.type)">
-            <br /><br />
-            <div v-for="(opt, i) of item.options" :key="i" class="form-check form-check-radio" >
-            <label  class="form-check-label">
-                <input class="form-check-input" :value="opt.val" type="radio" v-model="form[item.model]"> 
-                <span class="form-check-sign"></span> {{ opt.name }}
-            </label>
-            </div>
-        </template>
-
-        <!-- Datepicker -->
-        <datetime v-if="isDatepicker(item.type)" 
-                  :auto="dpConfig.auto"
-                  :class="dpConfig.theme"
-                  :format="dpConfig.format"
-                  :phrases="dpConfig.phrases"
-                  :input-class="dpConfig.classInput"
-                  v-model="form[item.model]" />
-
-    </div>
-</span>
+<div>
+  <br><br>
+  
+  <div class="columns is-mobile is-centered">
+    <div class="column is-6">
+      <div class="box">
+        <h2 class="title is-2">Form builder</h2>
+        <FormBuilder :fields="fields" />
+      </div> 
+    </div>  
+  </div> 
+  
+</div>
 </template>
 
-<script src="./backend.js"></script>
+<script>
+import FormBuilder from './form-builder.vue'
+import fields from './fields.js'
+
+export default {
+  components: {
+    FormBuilder
+  },
+
+  data: () => ({
+    fields
+  })
+}
+</script>
+
+
+<style>
+ @import url('https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.1/css/bulma.min.css');
+
+</style>
+
+
