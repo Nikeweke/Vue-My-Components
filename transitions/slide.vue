adfd56a9fe1c19c606105fb0277a30c910565895
Nikeweke <Driver3@meta.ua>
2018-10-12 09:47:42 +0300

    Create slide.vue

---
+++ transitions/slide.vue
@@ -0,0 +1,23 @@
+<template>
+  <transition name="step" mode="out-in">
+    <slot />
+  </transition>
+</template>
+
+
+
+<style lang="scss" scoped>
+  /* step change animation*/
+  .step-enter-active,
+  .step-leave-active{
+    transition: .23s;
+  }
+  .step-enter,
+  .step-leave-to{
+    opacity: 0;
+    transform: translateX(10%);
+  }
+  .step-leave-to{
+    transform: translateX(-10%);
+  }
+</style>
